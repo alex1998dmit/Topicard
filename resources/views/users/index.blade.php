329adcebd66eba79654410f117553bd52c10b83d
RukkiesMan <pavelekseev@gmail.com>
2019-04-03 15:22:55 +0300

    Change profile page

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,9 +1,6 @@
-
 @extends('layouts.app')
-
-    @section('content')
-
-    <div class="row">
+@section('content')
+    <!-- <div class="row">
         <div class="col-md-6">
             <div class="col-md-12">
                 <img src="{{asset('uploads/avatars/'. $user->avatar)}}" alt="avatar">
@@ -58,8 +55,28 @@
                 </div>
             </div>
         </div>
+    </div> -->
+    <div class="row mt-5">
+        <div class="col-sm-3">
+            <div class="row position-sticky">
+                <img class="profile__avatar rounded" src="{{asset('uploads/avatars/'. $user->avatar)}}" alt="avatar">
+                <h2 class="pt-3 mb-0"> {{ $user->name }}</h2>
+                <div class="w-100"></div>
+                <p>{{ $user->email }}</p>
+                <div class="w-100"></div>
+                <span class="h5">Карма: {{ $user->linkes }}</span>
+            </div>
+        </div>
+        <div class="col-sm-9">
+            <div class="row topics__all">
+                <h2 class="col">Ваши топики:</h2>
+            </div>
+            <div class="row topics__saved">
+                <h2 class="col">Сохраненные топики:</h2>
+            </div>
+            <div class="row topics__categories">
+                <h2 class="col">Избранные категории:</h2>
+            </div>
+        </div>
     </div>
-
-
-
-    @endsection
+@endsection
