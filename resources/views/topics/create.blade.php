9c8c9c147298cceca0768895a1031899690e30aa
RukkiesMan <pavelekseev@gmail.com>
2019-04-03 20:17:42 +0300

    Update create topic page

--- resources/views/topics/create.blade.php
+++ resources/views/topics/create.blade.php
@@ -1,32 +1,28 @@
 @extends('layouts.app')
     @section('content')
-        <form action="{{ route('topic.store') }}" method="post">
+        <form class="mt-3" action="{{ route('topic.store') }}" method="post">
             {{ csrf_field() }}
             <div class="row">
-                <div class="col-md-12">
-                        <div class="form-group" id="search_categories">
-                            <label for="categories_search">Выберите категорию</label>
-                            <input type="text" name="search" id="categories_search" class="form-control" placeholder="Введите название категории" />
-                        </div>
-                        <div class="checkboxes" id="checkboxes">
-                        </div>
+                <div class="form-group col-12">
+                    <input name="title" id="title" class="form-control" placeholder="Заголовок">
                 </div>
             </div>
-            <hr>
-            <div class="form-group">
-                    <label for="title">Title</label>
-                    <input name="title" id="title" class="form-control">
+            <div class="row">
+                <div class="form-group col-12" id="search_categories">
+                    <input type="text" name="search" id="categories_search" class="form-control" placeholder="Введите название категории" />
+                    <div class="checkboxes" id="checkboxes"></div>
                 </div>
-            <div class="row col-md-12">
-                <div class="row">
+            </div>
+            <hr class="w-100">
+            <div class="row">
+                <div class="col-12">
                     <div class="form-group">
-                        <textarea name="content" id="content" cols="150" rows="20" placeholder="Содержание топика" class="form-control"></textarea>
+                        <textarea name="content" id="content" cols="150" rows="10" placeholder="Содержание топика" class="form-control"></textarea>
                     </div>
                 </div>
             </div>
             <div class="row">
                 <div class="col-md-12 text-right">
-                    {{-- <a class="btn btn-danger mr-2" href="{{ route('user', ['id' => Auth::id()]) }}}">Удалить</a> --}}
                     <button class="btn btn-success" type="submit">Сохранить</button>
                 </div>
             </div>
