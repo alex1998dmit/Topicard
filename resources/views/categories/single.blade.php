4d77f857c8b65ad3a25644106943f204c84a5f4d
RukkiesMan <pavelekseev@gmail.com>
2019-04-04 20:19:08 +0300

    Add subscribe button

--- resources/views/categories/single.blade.php
+++ resources/views/categories/single.blade.php
@@ -1,9 +1,13 @@
 @extends('layouts.app')
 @section('content')
   <div class="row mt-4 mb-1">
-    <div class="col-12">
-        <h2>Топики категории: {{ $category->name }}</h2>
-    </div>
+    <h2 class="col">Топики категории: {{ $category->name }}</h2>
+    <div class="col-auto">
+      @if($category->is_subscribed_by_id())
+        <input class="btn btn-success unsubscribe_button" type="button" data-id="{{ $category->id }}" value="Отписаться">
+      @else
+        <input class="btn btn-primary subscribe_button" type="button" data-id="{{ $category->id }}" value="Подписаться">
+      @endif
   </div>
   <div class="row">
     @foreach($category->topic as $topic)
